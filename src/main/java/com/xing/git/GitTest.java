8859d589ded7018253c3162c353a71a907fe49cd
xing <2722617059@qq.com>
2023-06-04 14:02:54 +0800

    first update “hello github”

--- src/main/java/com/xing/git/GitTest.java
+++ src/main/java/com/xing/git/GitTest.java
@@ -10,6 +10,6 @@ package com.xing.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello GitHub!");
     }
 }
