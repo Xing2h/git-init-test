c3367cad9fd698032983793699795ba7da77e454
xing <2722617059@qq.com>
2023-06-04 13:56:51 +0800

    first commit

---
+++ src/main/java/com/xing/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.xing.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.xing.git
+ * Description:
+ * @Author: Xing
+ * @Create: 2023/6/4 - 13:55
+ * @Version: v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
